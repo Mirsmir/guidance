970f997ed3be18b62c55468014e61656e110464e
EEEE EEEE <urmom.com>
2024-06-08 01:50:55 -0400

    1:50

--- src/app.js
+++ src/app.js
@@ -17,18 +17,18 @@ function submit() {
     var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
     var sheet = spreadsheet.getActiveSheet();//Get the URL of the active spreadsheet and sheet
     var spreadsheetUrl = spreadsheet.getUrl();
-
+    var lastRow = sheet.getLastRow() - 2;
     var x = sheet.getRange(2, 1, sheet.getLastRow() - 1, 1);
     timestamps.push(x.getValues()); //will get all objects in range from script
     Logger.log(x);
-    emails.push(sheet.getRange(2, 2, sheet.getLastRow(), 1).getValues());
-    reasons.push(sheet.getRange(2, 4, sheet.getLastRow(), 1).getValues());
-    dates.push(sheet.getRange(2, 3, sheet.getLastRow(), 1).getValues());
-    teachers.push(sheet.getRange(2, 5, sheet.getLastRow(), 1).getValues());
+    emails.push(sheet.getRange(2, 2, sheet.getLastRow() - 1, 1).getValues());
+    reasons.push(sheet.getRange(2, 4, sheet.getLastRow() - 1, 1).getValues());
+    dates.push(sheet.getRange(2, 3, sheet.getLastRow() - 1, 1).getValues());
+    teachers.push(sheet.getRange(2, 5, sheet.getLastRow() - 1, 1).getValues());
 
-    Logger.log(teachers); //just checking it work
-
-    AddRecord(timestamps[timestamps.length - 1][0], emails[emails.length - 1][0], teachers[teachers.length - 1][0], dates[dates.length - 1][0], reasons[reasons.length - 1][0]);
+    Logger.log(lastRow + "wjwjjw"); //just checking it work
+    Logger.log(teachers);
+    AddRecord(timestamps[0][lastRow], emails[0][lastRow], teachers[0][lastRow], dates[0][lastRow], reasons[0][lastRow]);
 }
 
 function AddRecord(timestamp, email, teacher, date, reason) {
@@ -38,20 +38,19 @@ function AddRecord(timestamp, email, teacher, date, reason) {
     var teach2 = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1hCOdZW6d1kWZoFX9n8V5CyKuLtORy90PrCHxgF1R2TI/edit?gid=0#gid=0'); //dacey
     var teach3 = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1IGNt9RAm6-Re4BIWc7GJl7q-oL-tLDwE4WNLinb4sc0/edit?gid=0#gid=0'); //kim
 
-    Logger.log("Normalized teacher: " + teacher);
-
     Logger.log(teacher + " are u there?????")
-    // teach1.getSheetByName('Ms. Avery').appendRow(teacher);
+
     switch (String(teacher)) {
+
         case "Ms. Kim":
-            Logger.log("heelllo!!!!");
-            teach3.getSheetByName("Ms. Kim").appendRow(timestamp, email, date, reason);
+            teach3.getSheetByName("Ms. Kim").appendRow([timestamp[0], email[0], date[0], reason[0]]);
             break;
         case "Ms. Dacey":
-            teach2.getSheetByName("Ms. Dacey").appendRow(timestamp, email, date, reason);
+            teach2.getSheetByName("Ms. Dacey").appendRow([timestamp[0], email[0], date[0], reason[0]]);
             break;
         case "Ms. Avery":
-            teach1.getSheetByName("Ms. Avery").appendRow(timestamp, email, date, reason);
+            Logger.log(timestamp[0], email[0], date[0], reason[0])
+            teach1.getSheetByName("Ms. Avery").appendRow([timestamp[0], email[0], date[0], reason[0]]);
             break;
     }
 
@@ -64,6 +63,5 @@ function highlightLate() {
 
 // var data = ss.getSheetByName("Sheet1");
 // data.appendRow([name, date, teacher, new Date(), reason]); //dispaly, also add date requested, on the last row available
-
 
 
