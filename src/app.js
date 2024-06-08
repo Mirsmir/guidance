67234a3699e5b6e02184c486c3cacb0fc528dc81
EEEE EEEE <urmom.com>
2024-06-08 00:24:31 -0400

    12"24

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@
 var ss = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1btTT5Ns4p90v53gnw1ulDOMByTYSpj33zy9YO99G2kE/edit?resourcekey=&gid=375523362#gid=375523362');
 // const sheetName = Utilities.formatDate(day, Session.getScriptTimeZone(), "MM/dd/yyyy");
 
-var timestamp = [];
-var email = [];
-var reason = [];
-var date = [];
+var timestamps = [];
+var emails = [];
+var reasons = [];
+var dates = [];
 var teachers = [];
 
 function submit() {
@@ -19,16 +19,16 @@ function submit() {
     var spreadsheetUrl = spreadsheet.getUrl();
 
     var x = sheet.getRange(2, 1, sheet.getLastRow() - 1, 1);
-    timestamp.push(x.getValues()); //will get all objects in range from script
+    timestamps.push(x.getValues()); //will get all objects in range from script
     Logger.log(x);
-    email.push(sheet.getRange(2, 2, sheet.getLastRow(), 1).getValues());
-    reason.push(sheet.getRange(2, 4, sheet.getLastRow(), 1).getValues());
-    date.push(sheet.getRange(2, 3, sheet.getLastRow(), 1).getValues());
+    emails.push(sheet.getRange(2, 2, sheet.getLastRow(), 1).getValues());
+    reasons.push(sheet.getRange(2, 4, sheet.getLastRow(), 1).getValues());
+    dates.push(sheet.getRange(2, 3, sheet.getLastRow(), 1).getValues());
     teachers.push(sheet.getRange(2, 5, sheet.getLastRow(), 1).getValues());
 
     Logger.log(teachers); //just checking it work
 
-    AddRecord(timestamp[timestamp.length - 1][0], email[email.length - 1][0], teachers[teachers.length - 1][0], date[date.length - 1][0], reason[reason.length - 1][0]);
+    AddRecord(timestamps[timestamps.length - 1][0], emails[emails.length - 1][0], teachers[teachers.length - 1][0], dates[dates.length - 1][0], reasons[reasons.length - 1][0]);
 }
 
 function AddRecord(timestamp, email, teacher, date, reason) {
@@ -38,21 +38,20 @@ function AddRecord(timestamp, email, teacher, date, reason) {
     var teach2 = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1hCOdZW6d1kWZoFX9n8V5CyKuLtORy90PrCHxgF1R2TI/edit?gid=0#gid=0'); //dacey
     var teach3 = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1IGNt9RAm6-Re4BIWc7GJl7q-oL-tLDwE4WNLinb4sc0/edit?gid=0#gid=0'); //kim
 
-    teacher = String(teacher);
     Logger.log("Normalized teacher: " + teacher);
 
     Logger.log(teacher + " are u there?????")
     // teach1.getSheetByName('Ms. Avery').appendRow(teacher);
-    switch (teacher) {
+    switch (String(teacher)) {
         case "Ms. Kim":
             Logger.log("heelllo!!!!");
-            teach3.getSheetByName("Ms. Kim").appendRow([timestamp, email, date, reason]);
+            teach3.getSheetByName("Ms. Kim").appendRow(timestamp, email, date, reason);
             break;
         case "Ms. Dacey":
-            teach2.getSheetByName("Ms. Dacey").appendRow([timestamp, email, date, reason]);
+            teach2.getSheetByName("Ms. Dacey").appendRow(timestamp, email, date, reason);
             break;
         case "Ms. Avery":
-            teach1.getSheetByName("Ms. Avery").appendRow([timestamp, email, date, reason]);
+            teach1.getSheetByName("Ms. Avery").appendRow(timestamp, email, date, reason);
             break;
     }
 
