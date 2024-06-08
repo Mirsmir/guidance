ce5f7b79ba7932ddad8030db96ac43b4f12a68e4
EEEE EEEE <urmom.com>
2024-06-07 23:57:05 -0400

    11:56

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // var ss = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1hCOdZW6d1kWZoFX9n8V5CyKuLtORy90PrCHxgF1R2TI/edit#gid=0');
-
+//awhjbawbhdhbawbhahjwdbhjabhjdawdhj
 var ss = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1btTT5Ns4p90v53gnw1ulDOMByTYSpj33zy9YO99G2kE/edit?resourcekey=&gid=375523362#gid=375523362');
 // const sheetName = Utilities.formatDate(day, Session.getScriptTimeZone(), "MM/dd/yyyy");
 
@@ -7,7 +7,7 @@ var timestamp = [];
 var email = [];
 var reason = [];
 var date = [];
-var teacher = [];
+var teachers = [];
 
 function accessemailheet() { //this is really time consuming and stupid, doing it for the crispy UI
     var lastRow = ss.getLastRow();
@@ -23,55 +23,42 @@ function accessemailheet() { //this is really time consuming and stupid, doing i
     return (email);
 }
 
+
+
 function submit() {
     // appendCounter++;
 
     Logger.log("here");
 
     var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet = spreadsheet.getActiveSheet();
-    // Get the URL of the active spreadsheet
+    var sheet = spreadsheet.getActiveSheet();//Get the URL of the active spreadsheet and sheet
     var spreadsheetUrl = spreadsheet.getUrl();
 
-    // var something = SpreadsheetApp.getActiveSheet();
-
-    // var sheet = SpreadsheetApp.openById('1btTT5Ns4p90v53gnw1ulDOMByTYSpj33zy9YO99G2kE'); //will redirect to the link of the form 
     var x = sheet.getRange(2, 1, sheet.getLastRow() - 1, 1);
     timestamp.push(x.getValues()); //will get all objects in range from script
     Logger.log(x);
     email.push(sheet.getRange(2, 2, sheet.getLastRow(), 1).getValues());
     reason.push(sheet.getRange(2, 4, sheet.getLastRow(), 1).getValues());
     date.push(sheet.getRange(2, 3, sheet.getLastRow(), 1).getValues());
-    teacher.push(sheet.getRange(2, 5, sheet.getLastRow(), 1).getValues());
+    teachers.push(sheet.getRange(2, 5, sheet.getLastRow(), 1).getValues());
 
-    // var count = responses.length; //the length responses
-    // var latestResp = timestamp.length - 1; //last response
-    // var questionItems = latestResp.getItemResponses();
-    // array[0].push(accessemailsheet());
+    Logger.log(teachers); //just checking it work
 
-    // for (var i = 0; i < 5; i++) {
-
-    // var questionItem = questionItems[i];
-    // var prompt = questionItem.getItem().getTitle(); //the title of the question in the google form
-    // var ans = questionItem.getResponse();
-    Logger.log(timestamp, " ", email, " ", teacher); //just checking it work
-    // Logger.log(ans);
-    // array.push(ans); //now actually updating array that wil update the google sheet
-    // }
-    AddRecord(timestamp[timestamp.length - 1], email[email.length - 1], teacher[teacher.length - 1], date[date.length - 1], reason[reason.length - 1]);
-    // var length = array.length;
+    AddRecord(timestamp[timestamp.length - 1][0], email[email.length - 1][0], teachers[teachers.length - 1][0], date[date.length - 1][0], reason[reason.length - 1][0]);
 }
 
 function AddRecord(timestamp, email, teacher, date, reason) {
+    Logger.log("hreer");
 
     var teach1 = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1qr3CVw9SAR-xFSaGGi9gpSXyujdP9pb3wbyHGdF89DE/edit?gid=0#gid=0'); //avery
     var teach2 = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1hCOdZW6d1kWZoFX9n8V5CyKuLtORy90PrCHxgF1R2TI/edit?gid=0#gid=0'); //dacey
     var teach3 = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1IGNt9RAm6-Re4BIWc7GJl7q-oL-tLDwE4WNLinb4sc0/edit?gid=0#gid=0'); //kim
 
     Logger.log(teacher + " are u there?????")
-    ss.getSheetByName('Sheet1').appendRow([teacher]);
+    // teach1.getSheetByName('Ms. Avery').appendRow(teacher);
     switch (teacher) {
         case "Ms. Kim":
+            Logger.log("heelllo!!!!");
             teach3.getSheetByName("Ms. Kim").appendRow([timestamp, email, date, reason]);
             break;
         case "Ms. Dacey":
