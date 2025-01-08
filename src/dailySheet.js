eba542c2334a7a394204bca106fa152417106310
MirSmir <116317864+Mirsmir@users.noreply.github.com>
2025-01-08 16:48:43 -0500

    create sheets

--- src/dailySheet.js
+++ src/dailySheet.js
@@ -27,32 +27,8 @@ function runAdd() {
 @pre: n/a
 @post: create new sheet with week names
 */
-function createSheets(ssx) {
-    var weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-    Logger.log("Deleting and replacing");
-    // deletion process, if same name found, delete it.
-    weekdays.forEach(function (day) { //beautiful for each
-        var s = ssx.getSheetByName(day);
-        if (s) {
-            ssx.deleteSheet(s); //if it finds the same name
-        }
-    });
-
-    //now that its ensured that the previous ones are gone, make a new batch
-
-    const rangeToCopy = ssx.getSheetByName("Template").getDataRange();
-    weekdays.forEach(function (day) {
-        const newSheet = ssx.insertSheet(day);
-        Logger.log("It should have made a new one by now");
-        rangeToCopy.copyTo(newSheet.getRange(1, 1));
-    });
-
-}
-
-function updateSheets() {
-    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    const sheets = spreadsheet.getSheets();
+function createSheets(sheets) {
 
     // Get today's date
     const today = new Date();
@@ -61,38 +37,27 @@ function updateSheets() {
     const yesterdayName = formatDate(yesterday);
     const newSheetName = formatDate(new Date(today.setDate(today.getDate() + 14))); //onec again will change month and year if necessary
 
-    // Delete the sheet for "yesterday" if it exists
-    for (let i = 0; i < sheets.length; i++) {
-        if (sheets[i].getName() === yesterdayName) {
-            spreadsheet.deleteSheet(sheets[i]);
-            break;
-        }
-    }
+    yesteryear(1, yesterdayName);
 
-
-
-    // Add a new sheet for "today + 2 weeks"
-    if (!spreadsheet.getSheetByName(newSheetName)) {
+    if (!spreadsheet.getSheetByName(newSheetName)) { //make sure theres no duplicates 
         const newSheet = spreadsheet.insertSheet(newSheetName);
-        newSheet.getRange("A1").setValue(`Sheet for ${newSheetName}`);
     }
 }
 
-//  format  date as "YYYY-MM-DD"
-function formatDate(date) {
+function formatDate(date) {//  format  date as "YYYY-MM-DD"
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const day = date.getDate().toString().padStart(2, "0");
     return `${year}-${month}-${day}`;
 }
 
-function yesteryear() {
+function yesteryear(i, yesterdayName) {
 
     if (sheets[i].getName() === yesterdayName) {
         spreadsheet.deleteSheet(sheets[i]);
         return;
     }
 
-    yesteryear();
+    yesteryear(i + 1, yesterdayName);
 
 }
